dc50335e2d0dc755fef48daeea06a6341d0c2ae0
Refredo <artem2006.br@gmail.com>
2023-12-08 14:42:30 +0300

    logging added

--- first_task/scripts/python/script.py
+++ first_task/scripts/python/script.py
@@ -1,3 +1,5 @@
+import logging
+
 import pandas as pd
 import psycopg2 as ps
 from sqlalchemy import create_engine
@@ -8,6 +10,12 @@ import os
 
 import json
 from typing import Dict
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+
+logger = logging.getLogger("data loading")
+
 
 load_dotenv()
 
@@ -20,40 +28,57 @@ connection = ps.connect(
     )
 
 def create_tables() -> None:
-    cursor = connection.cursor()
-    cursor.execute(open('../sql/schema.sql', 'r').read())
-    connection.commit()
-    cursor.close()
+    try:
+        cursor = connection.cursor()
+        cursor.execute(open('../sql/schema.sql', 'r').read())
+        connection.commit()
+
+        logger.info('Tables were created successfully')
+
+    except Exception as e:
+        logger.error(f"can't execute create tables by exception: {e}")
+    
+    finally:
+        cursor.close()
 
 def execute_queries(save_format='json') -> None:
     cursor = connection.cursor()
     
-    # Список комнат и количество студентов в каждой из них
-    cursor.execute(open('../sql/first_query.sql', 'r').read())
-    lst_first = cursor.fetchall()
+    try:
+        # Список комнат и количество студентов в каждой из них
+        cursor.execute(open('../sql/first_query.sql', 'r').read())
+        lst_first = cursor.fetchall()
 
-    # 5 комнат, где самый маленький средний возраст студентов
-    cursor.execute(open('../sql/second_query.sql', 'r').read())
-    lst_second = cursor.fetchall()
+        # 5 комнат, где самый маленький средний возраст студентов
+        cursor.execute(open('../sql/second_query.sql', 'r').read())
+        lst_second = cursor.fetchall()
 
-    #5 комнат с самой большой разницей в возрасте студентов
-    cursor.execute(open('../sql/third_query.sql', 'r').read())
-    lst_third = cursor.fetchall()
+        #5 комнат с самой большой разницей в возрасте студентов
+        cursor.execute(open('../sql/third_query.sql', 'r').read())
+        lst_third = cursor.fetchall()
 
-    # Список комнат где живут разнополые студенты
-    cursor.execute(open('../sql/fourth_query.sql', 'r').read())
-    lst_fourth = cursor.fetchall()
+        # Список комнат где живут разнополые студенты
+        cursor.execute(open('../sql/fourth_query.sql', 'r').read())
+        lst_fourth = cursor.fetchall()
 
-    if save_format == 'json':
-        save_in_json(data=lst_first, keys=['room', 'amount'], fileName='../../output_data/firstquery.json')
-        save_in_json(data=lst_second, keys=['id', 'room'], fileName='../../output_data/secondquery.json')
-        save_in_json(data=lst_third, keys=['id', 'room'], fileName='../../output_data/thirdquery.json')
-        save_in_json(data=lst_fourth, keys=['id', 'room'], fileName='../../output_data/fourthquery.json')
-    else:
-        print('there is no such format of save file')
+        if save_format == 'json':
+            save_in_json(data=lst_first, keys=['room', 'amount'], fileName='../../output_data/firstquery.json')
+            save_in_json(data=lst_second, keys=['id', 'room'], fileName='../../output_data/secondquery.json')
+            save_in_json(data=lst_third, keys=['id', 'room'], fileName='../../output_data/thirdquery.json')
+            save_in_json(data=lst_fourth, keys=['id', 'room'], fileName='../../output_data/fourthquery.json')
+        else:
+            print('there is no such format of save file')
 
-    for row in cursor.fetchall():
-        print(row)
+        for row in cursor.fetchall():
+            print(row)
+
+        logger.info('queries were completed successfully')
+            
+    except Exception as e:
+        logger.error(f'Error while executing queries. Error: {e}')
+    
+    finally:
+        cursor.close()
     
 def save_in_json(data, keys, fileName) -> None:
     lst = [dict(zip(keys, values)) for values in data]
@@ -65,10 +90,18 @@ def save_in_json(data, keys, fileName) -> None:
 def extract_data(students_path='../../data/students.json', rooms_path='../../data/rooms.json') -> Dict:
     students = None
     rooms = None
-    with open(students_path, 'r') as file:
-        students = json.load(file)
-    with open(rooms_path, 'r') as file:
-        rooms = json.load(file)
+
+    try:
+        with open(students_path, 'r') as file:
+            students = json.load(file)
+        with open(rooms_path, 'r') as file:
+            rooms = json.load(file)
+
+        logger.info('data were extracted successfully')
+
+    except Exception as e:
+        logger.error('Error while try to extarct data from file. Error: {e}')
+        
     return students, rooms
 
 def transform_data(students, rooms) -> tuple:
@@ -87,18 +120,25 @@ def load_data(df_students, df_rooms) -> None:
     students_table = 'students'
     rooms_table = 'rooms'
     
-    host = 'localhost',
-    database = os.getenv('POSTGRES_DB'),
-    user = os.getenv('POSTGRES_USER'),
-    password = os.getenv('POSTGRES_PASSWORD'),
+    host = 'localhost'
+    database = os.getenv('POSTGRES_DB')
+    user = os.getenv('POSTGRES_USER')
+    password = os.getenv('POSTGRES_PASSWORD')
     port = 5432
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+    try:
+        engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
     
-    df_students.to_sql(students_table, engine, if_exists='append', index=False)
-    df_rooms.to_sql(rooms_table, engine, if_exists='append', index=False)
+        df_students.to_sql(students_table, engine, if_exists='append', index=False)
+        df_rooms.to_sql(rooms_table, engine, if_exists='append', index=False)
 
-    engine.dispose()
+        logger.info('data were loaded successfully')
+
+    except Exception as e:
+        logger.error(f'Error while try to load data into database. Error: {e}')
+    
+    finally:
+        engine.dispose()
       
 
 def main():
